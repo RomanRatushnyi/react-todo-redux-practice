2e47f680f7d650de69157f2886036f629891e05a
Nataliia Yakovenko <yakovenkonatali999@gmail.com>
2025-10-10 19:08:44 +0300

    Correct accesstoken

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,34 @@
-import { Routes, Route, Navigate, useNavigate } from 'react-router-dom'
-import { useAppSelector } from './store/hooks'
-import LoginPage from './pages/LoginPage'
-import NotFoundPage from './pages/NotFoundPage'
-import TodosPage from './pages/TodosPage'
-import './App.css'
+import { Routes, Route, Navigate, useNavigate } from "react-router-dom";
+import { useAppSelector } from "./store/hooks";
+import LoginPage from "./pages/LoginPage";
+import NotFoundPage from "./pages/NotFoundPage";
+import TodosPage from "./pages/TodosPage";
+import "./App.css";
 
 function App() {
-  const { isAuthenticated } = useAppSelector((state) => state.auth)
-  const navigate = useNavigate()
+  const { isAuthenticated } = useAppSelector((state) => state.auth);
+  const navigate = useNavigate();
 
-  if (!isAuthenticated) {
-    return <LoginPage />
+  function hasCookie(name: string) {
+    return document.cookie
+      .split("; ")
+      .some((row) => row.startsWith(name + "="));
+  }
+
+  if (!isAuthenticated && !hasCookie("access_token")) {
+    return <LoginPage />;
   }
 
   return (
     <Routes>
       <Route path="/" element={<TodosPage />} />
       <Route path="/todos" element={<Navigate to="/" replace />} />
-      <Route path="*" element={<NotFoundPage onGoHome={() => navigate('/')} />} />
+      <Route
+        path="*"
+        element={<NotFoundPage onGoHome={() => navigate("/")} />}
+      />
     </Routes>
-  )
+  );
 }
 
-export default App
+export default App;
